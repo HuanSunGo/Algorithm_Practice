3b4397dd225b1fca0c9acf347e597cc0008c18d4
Huan Sun <hs3240@columbia.edu>
2022-07-14 12:34:49 -0400

    add recursion

--- Recursion.py
+++ Recursion.py
@@ -1,1 +1,17 @@
 ## Q1: Fibonacci Sequence
+"""
+[0,1,1,2,3,5,8,13,21,...]
+"""
+def fib(n):
+    a = 0 
+    b = 1
+    if n == 1: 
+        print(a)
+    else:
+        for i in range(2,n):
+            c = a + b 
+            a = b 
+            b = c 
+            print(c) # for debug
+
+fib(6)
