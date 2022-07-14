9f20e02fa51a7a5befe1330f261164e4fb491626
Huan Sun <hs3240@columbia.edu>
2022-07-14 16:46:29 -0400

    add fibonacci

--- Recursion.py
+++ Recursion.py
@@ -1,8 +1,14 @@
-## Q1: Fibonacci Sequence
+## Q1: Fibonacci Sequence --> Iteration Method
 """
-[0,1,1,2,3,5,8,13,21,...]
+[0,1,1,2,3,5,8,13,21,...] 
+
+Time Complexity: O(n)
+Space Complexity: O(1)
 """
-def fib(n):
+from typing import Type
+
+
+def fib_iteration(n):
     a = 0 
     b = 1
     if n == 1: 
@@ -13,5 +19,59 @@ def fib(n):
             a = b 
             b = c 
             print(c) # for debug
+    return c 
+print(fib_iteration(6))
 
-fib(6)
+## Q2: Fibonacci Sequence --> Recursion Method
+"""
+Recursion: Looks like the functions call itself, yet essentially is boilling down a big problem to smaller ones.
+
+Time Complexity: O(2^n)
+Space Complexity: O(n) - call stack cost
+
+Really bad performance unless use the following cache library function.
+# from functools import lru_cache
+# @lru_cache(maxsize = 1000) 
+"""
+
+
+def fib_recursion(n):
+    # base case: the smallest problem to solve 
+    if n <= 1:
+        return n 
+    # recursive rule: how to make the problem smaller 
+    return fib_recursion(n-1) + fib_recursion(n-2)
+
+for n in range(1,30):
+    print(n, ":", fib_recursion(n))
+
+
+## Q3: Optimized Fibonacci --> Using Memory
+
+fibonacci_cache={}
+
+def fib_memory(n):
+    # first check the type of input
+    if type(n) != int:
+        raise TypeError("n must be a positve int")
+    if n < 1: 
+        raise ValueError("n must be a positve int")
+
+    # if we have cached the value, then return it.
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+    
+    #compute the Nth term
+    if n == 1: 
+        value = 1
+    elif n == 2:
+        value = 1
+    elif n > 2: 
+        value = fib_memory(n-1) + fib_memory(n-2)
+
+    # cache the value and return it 
+    fibonacci_cache[n] = value 
+    return value 
+
+for n in range(1,101):
+    print(n, ":", fib_memory(n))
