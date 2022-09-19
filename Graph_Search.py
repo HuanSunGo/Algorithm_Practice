c88b79fccf372d784f173e3bb4aef7fd359dff56
Huan Sun <hs3240@columbia.edu>
2022-09-18 22:54:20 -0400

    add graph search

---
+++ Graph_Search.py
@@ -0,0 +1,25 @@
+"""
+Graph Definition: 
+A set of edges E, with every edge in E connects two vertices.
+
+Application: 
+- web crawling: V -- page/website, E -- link
+- social networking V -- people, E -- connection 
+
+"""
+
+# Traverse: breadth first search(BFS), pseudo code 
+def bfs(graph,s):
+    """
+    TC: O(V+E*T)
+    """
+    frontier = [s]
+    has_seen = set(s)
+    while frontier:
+        next = []
+        for u in frontier: 
+            for v in neighbors(u):
+                if v not in has_seen:
+                    next.append(v)
+                    has_seen.add(v)
+        frontier = next 
